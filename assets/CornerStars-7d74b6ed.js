2f984a5a61d4131e8a7f74a710e93489f95623a9
WanSouu <wansolek@gmail.com>
2023-10-22 17:04:43 +0100

    Sort improvements

---
+++ assets/CornerStars-7d74b6ed.js
@@ -0,0 +1,1 @@
+import{r as e,m as s}from"./index-14105445.js";import{S as n,c as m,a as u}from"./Star-dbdfdcac.js";const p=(r=Math.round(Math.random()))=>[3-r,r];function x({starPreset:r}){const[t,c]=e.useState((r??p()).map(a=>new n(a)));return s.jsx("p",{className:m.starryArea,"data-foreground":!0,children:t.map((a,o)=>s.jsx(u,{starClass:a,foreground:!0},o))})}export{x as C};
